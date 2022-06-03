07a196edc31fdd5b09882f5475d4c6da235c99c0
newlandslucas <lucasnewlands.m@gmail.com>
2022-06-03 15:46:44 -0300

    Initial Commit

---
+++ AuthenticatorDemo/AuthenticatorDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticatorDemoApp.swift
+//  AuthenticatorDemo
+//
+//  Created by Lucas Machado (Estudos) on 03/06/22.
+//
+
+import SwiftUI
+
+@main
+struct AuthenticatorDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
