e407c25a303320a100976e0be74c8eeb4897b8a4
newlandslucas <lucasnewlands.m@gmail.com>
2022-06-06 17:13:26 -0300

    feat: Authentication with FaceID | TouchID

--- AuthenticatorDemo/Views/ContentView.swift
+++ AuthenticatorDemo/Views/ContentView.swift
@@ -8,12 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var authenticationManager = AuthenticationManager()
+    
     var body: some View {
         VStack {
             LoginView()
+                .environmentObject(authenticationManager)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .edgesIgnoringSafeArea(.all)
+        .alert( isPresented: $authenticationManager.showAlert) {
+            Alert(title: Text("error"), message: Text( authenticationManager.errorDescription ?? "Error trying to login with credentials, please try again"), dismissButton: .default(Text("Ok")))
+        }
     }
 }
 
